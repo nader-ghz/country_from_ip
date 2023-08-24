39ae19ae163a92673329019067b6a93c41598e93
Nader <ghazouani.nader@gmail.com>
2023-08-24 21:49:02 +0100

    another test

--- app/api/country/route.js
+++ app/api/country/route.js
@@ -1,7 +1,7 @@
 import {NextResponse} from "next/server";
 import maxmind from 'maxmind';
 export async function GET(req, res) {
-    const geolocationDatabase = await maxmind.open('/assets/GeoLite2/GeoLite2-Country.mmdb');
+    const geolocationDatabase = await maxmind.open('../../../assets/GeoLite2/GeoLite2-Country.mmdb');
     // Get the user's IP address from the request context
     const userIP = req.headers['x-forwarded-for'] || '102.156.93.242';
     const geolocationResult = geolocationDatabase.get(userIP);

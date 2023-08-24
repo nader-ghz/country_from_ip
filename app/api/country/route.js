c0acb2b28b8ef43828bde5551a9cd3d56c596808
Nader <ghazouani.nader@gmail.com>
2023-08-25 00:59:35 +0100

    get user country

--- app/api/country/route.js
+++ app/api/country/route.js
@@ -1,11 +1,35 @@
 import {NextResponse} from "next/server";
 import maxmind from 'maxmind';
+import {WebServiceClient} from "@maxmind/geoip2-node";
+
 export async function GET(req, res) {
-    const geolocationDatabase = await maxmind.open('./assets/GeoLite2/GeoLite2-Country.mmdb');
-    // Get the user's IP address from the request context
-    const userIP = req.headers['x-forwarded-for'] || '102.156.93.242';
-    const geolocationResult = geolocationDatabase.get(userIP);
-    return NextResponse.json({
-        test : geolocationResult
-    })
+
+
+    try {
+        const parsedUrl = new URL(req.url, `https://${req.headers.host}`);
+        const userIP = parsedUrl.searchParams.get('address');
+        const license = process.env.MAXMIND_LICENSE_KEY ;
+        const account_id = process.env.ACCOUNT_ID;
+        const option = {
+            'host' : 'geolite.info'
+        };
+        const client = new WebServiceClient(account_id, license,option);
+        // Get the user's IP address from the request context
+        try {
+            const country = await client.city(userIP)
+            return NextResponse.json({
+                Location : country
+            })
+        }catch (e){
+             console.log('error')
+            return NextResponse.json({
+                error :'IP_ADDRESS_INVALID',
+            })
+        }
+
+    }catch (e){
+         console.log(e)
+    }
+
+
 }

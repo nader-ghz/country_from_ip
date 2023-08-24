53b254b7a67f7f0163029e54efcec5eb0af90580
Nader <ghazouani.nader@gmail.com>
2023-08-24 21:42:45 +0100

    ejfksd

---
+++ app/api/country/route.js
@@ -0,0 +1,11 @@
+import {NextResponse} from "next/server";
+import maxmind from 'maxmind';
+export async function GET(req, res) {
+    const geolocationDatabase = await maxmind.open('./assets/GeoLite2/GeoLite2-Country.mmdb');
+    // Get the user's IP address from the request context
+    const userIP = req.headers['x-forwarded-for'] || '102.156.93.242';
+    const geolocationResult = geolocationDatabase.get(userIP);
+    return NextResponse.json({
+        test : 'hi'
+    })
+}
